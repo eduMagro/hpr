c8bfe5fa6547a9e8265747547a0e683c409fa49e
eduMagro <edumagrolemus@hotmail.com>
2025-01-13 16:40:25 +0100

    commit3

--- routes/auth.php
+++ routes/auth.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
 
-
-	    Route::get('/', [AuthenticatedSessionController::class, 'create'])
-        ->name('login');
-
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
 
