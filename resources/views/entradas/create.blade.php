cc7cd36e82bfceecd91081cad061eb4e47938288
eduMagro <edumagrolemus@hotmail.com>
2025-01-14 08:51:40 +0100

    klfasd

--- resources/views/entradas/create.blade.php
+++ resources/views/entradas/create.blade.php
@@ -145,6 +145,8 @@
         </div>
     </div>
 
+    <!-- Incluir el JS de Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
     <!-- Script de Validación y Generación Dinámica de Campos -->
     <script>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,12 +15,8 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 
         <!-- Bootstrap (solo CSS si es necesario) -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+        {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"> --}}
 
-        
-    <!-- Incluir el JS de Bootstrap -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    
     <!-- Tailwind CSS -->
     <script src="https://cdn.tailwindcss.com"></script>
 
