79f1db9ea7640ffd64fb1f12e270824b35e49785
eduMagro <edumagrolemus@hotmail.com>
2025-01-14 12:46:49 +0100

    restauro app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
 
     <title>{{ config('app.name', 'Página principal - HPR') }}</title>
 
-    <!-- Fonts -->
+    {{-- <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
@@ -16,10 +16,9 @@
 
     <!-- Bootstrap (solo CSS si es necesario) -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
- <!-- Tailwind CSS (solo si es necesario) -->
- @if(isset($useTailwind) && $useTailwind)
- <script src="https://cdn.tailwindcss.com"></script>
-@endif
+    <!-- Tailwind CSS -->
+    <script src="https://cdn.tailwindcss.com"></script>
+
     <!-- Chart.js -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
@@ -27,10 +26,29 @@
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
 
     <!-- Estilos Personalizados -->
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}"> --}}
+      
+      <!-- Agregar Bootstrap desde CDN -->
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+      {{-- para graficos --}}
+      <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+  
+      <!-- ESTILOS -->
+      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+          integrity="sha384-oBqDVmMz4fnFO9gybK3zs7hcm9Kmdt6Q5Ppp3HLzE5ptlTmz3Ovv9r3e2b9Kr5Tk" crossorigin="anonymous">
+      </script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"
+          integrity="sha384-pzjw8f+ua7Kw1TIq0lLvYdpP1fo/+/fo+2Vxwz3bce4C/qvkeBxjtvxDsw7fG5p5" crossorigin="anonymous">
+      </script>
+  <script src="https://cdn.tailwindcss.com"></script>
+  
+      <!-- Scripts -->
+      <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
+  <script src="https://cdn.jsdelivr.net/npm/library.js"></script>
+       <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 	
 	<!-- JAVASCRIPT -->
-
 	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     <!-- Iconos y Configuración PWA -->
