b95919f379870584c11a9550d07d1a88ac277153
eduMagro <edumagrolemus@hotmail.com>
2025-01-21 23:26:26 +0100

    usuarios corregido

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -53,8 +53,6 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);
-
         return redirect(route('users.index', absolute: false));
     }
 }
