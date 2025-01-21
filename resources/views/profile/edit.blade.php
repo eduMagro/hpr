895aacb4a46cfdbafe1b05432caccf2eafe69d9c
eduMagro <edumagrolemus@hotmail.com>
2025-01-21 22:42:05 +0100

    usuarios v14

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -25,7 +25,8 @@
                             </p>
                         </header>
 
-                        <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
+                        <form method="post" action="{{ route('profile.update', ['id' => $user->id]) }}"
+                            class="mt-6 space-y-6">
                             @csrf
                             @method('patch')
 
