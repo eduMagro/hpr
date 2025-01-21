6a2aff994daba795bd667706fa2d159c91a167f3
eduMagro <edumagrolemus@hotmail.com>
2025-01-21 22:32:41 +0100

    usuairos v9

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,9 @@ Route::get('/dashboard', function () {
 
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::get('/users/{id}/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/users/{id}', [ProfileController::class, 'update'])->name('profile.update');
+
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
