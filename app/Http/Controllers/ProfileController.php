1a9211eac932daebd0a4e47eb3a552feb0f5757e
eduMagro <edumagrolemus@hotmail.com>
2025-03-06 17:08:02 +0100

    arreglo seleccion multiple en asignaciones de turno

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -239,8 +239,6 @@ class ProfileController extends Controller
             'turnos',
             'faltasInjustificadas',
             'faltasJustificadas',
-            'mediaFaltasJustificadas',
-            'mediaFaltasInjustificadas',
             'diasBaja'
         ));
     }
