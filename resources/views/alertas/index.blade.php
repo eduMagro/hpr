fb63ae0dbbd2db6dc5fdc8d7bfc1b5b44fedb11d
eduMagro <edumagrolemus@hotmail.com>
2025-06-05 00:04:29 +0200

    manda mensajes a rrhh, jose manuel sebastian y yo

--- resources/views/alertas/index.blade.php
+++ resources/views/alertas/index.blade.php
@@ -64,7 +64,7 @@
                             </div>
                         @else
                             <!-- Para usuarios que no son de oficina: ocultamos los campos -->
-                            <input type="hidden" name="enviar_a_departamentos" value="RRHH,Producción,Programador">
+                            <input type="hidden" name="enviar_a_departamentos" value="RRHH">
                         @endif
 
                         <div class="flex justify-end space-x-2">
