0b88ab94fdb1861f9f443652bf7e4fb6e2889806
eduMagro <edumagrolemus@hotmail.com>
2025-07-31 08:37:38 +0200

    corrijo otra vez boton

--- resources/views/components/ficha-trabajador.blade.php
+++ resources/views/components/ficha-trabajador.blade.php
@@ -89,7 +89,8 @@
         <div class="mt-6 border-t pt-6">
             <h3 class="text-lg font-semibold text-gray-700 mb-2">📥 Descargar mis nóminas</h3>
             <form action="{{ route('nominas.descargarMes') }}" method="GET"
-                class="flex flex-wrap items-center gap-3 max-w-md">
+                class="flex flex-wrap items-center gap-3 max-w-md"
+                onsubmit="this.querySelector('button').disabled = true;">
                 @csrf
 
                 <input type="month" name="mes_anio" id="mes_anio" required
