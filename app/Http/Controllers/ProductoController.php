f1e1c64f8d511408c25943ea30709d0e68ddba65
eduMagro <edumagrolemus@hotmail.com>
2025-09-22 15:07:39 +0200

    cambios publico

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -523,7 +523,6 @@ class ProductoController extends Controller
 
         return back()->with('success', '✅ Producto actualizado correctamente.');
     }
-
     private function marcarComoConsumido(Producto $producto)
     {
         $producto->peso_stock = 0;
